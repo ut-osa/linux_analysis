982caf6d7900f9e231a3d271444649f1668181fe
osh <osh@0a08b987-4bf4-4c34-b5fc-5002ad2b9887>
2010-04-02 23:08:23 +0000

    Brush this up to code
    git-svn-id: svn+ssh://plant/filer/osa/svn-hype/osck@45 0a08b987-4bf4-4c34-b5fc-5002ad2b9887

--- cil-tools/typeapi.ml
+++ cil-tools/typeapi.ml
@@ -26,56 +26,68 @@ let kptr_typ t =
       incr n_kptrs;
       k
 
-
-let print_pre_def g =
-   dumpGlobal !printerForMaincil !out_pre_defs g
-
-let print_defn g =
-   dumpGlobal !printerForMaincil !out_struct_list g
-
 let rec make_kptr t =
    match t with
-   | TPtr (t, a) -> (
-      match t with
+   | TPtr (pt, a) -> let baset = unrollType pt in (
+      match baset with
       | TFun _ -> t
       | _ -> 
-         let t = make_kptr t in
-         let KPtr (id, ot, kt) = kptr_typ t in
+         let pt = make_kptr pt in
+         let KPtr (id, ot, kt) = kptr_typ pt in
          kt
    )
    | TArray (t, e, a) -> TArray (make_kptr t, e, a)
    | t -> t
 
+
+let anon_field f =
+   Str.string_match (Str.regexp_string "__annonCompField") f.fname 0
+
+let anon_comp c =
+   Str.string_match
+      (Str.regexp "__anon\\(struct\\|union\\)____missing_field_name_")
+      c.cname 0
+
 let safe_cpp name =
    match name with
-   | "private" -> "_private"
+   | "private" -> "_C_private"
+   | "class" -> "_C_class"
+   | "this" -> "_C_this"
+   | "true" -> "_C_true"
+   | "false" -> "_C_false"
+   | "not" -> "_C_not"
+   | "typename" -> "_C_typename"
    | _ -> name
 
-let rec sub_kptrs fields comp =
-   match fields with
-   | fi :: rest ->
-      (safe_cpp fi.fname, make_kptr fi.ftype, fi.fbitfield, fi.fattr, fi.floc) :: (sub_kptrs rest comp)
-   | [] -> []
-
-class makeKptrs = object(self)
-   inherit nopCilVisitor
-   method vglob (g : global) =
-      match g with
-      | GCompTag (comp, loc) ->
-         ChangeTo [
-            (GCompTag (mkCompInfo comp.cstruct comp.cname
-                       (sub_kptrs comp.cfields) comp.cattr, loc))
-         ]
-      | g -> DoChildren
+(* Cil printer class that expands anonymous unnamed inner struct/union *)
+class anonPrinterClass = object(self)
+   inherit defaultCilPrinterClass as super
+   method pFieldDecl _ (f : fieldinfo) =
+      match f.ftype with
+      | TComp (comp, attrs) when anon_field f -> 
+         let comp_new = copyCompInfo comp "" in
+         super#pGlobal () (GCompTag (comp_new, f.floc))
+      | _ -> super#pFieldDecl () f
 end
+let anonPrinter = new anonPrinterClass
+
+let print_pre_def g =
+   dumpGlobal !printerForMaincil !out_pre_defs g
+
+let print_defn g =
+   match g with
+   | GCompTag (comp, loc) when anon_comp comp -> ()
+   | GCompTag (comp, loc) -> 
+      dumpGlobal anonPrinter !out_struct_list g
+   | _ -> dumpGlobal !printerForMaincil !out_struct_list g
 
 class printTypeDefs = object(self)
    inherit nopCilVisitor
    method vglob (g : global) =
       match g with
-      | GType (t,loc) -> (match t.tname with
-         | "bool" -> DoChildren
-         | _ -> print_pre_def g; DoChildren)
+      | GType ({tname="bool"}, _) -> DoChildren
+      | GType (_, _) -> print_pre_def g; DoChildren
+
       | GEnumTag _ -> print_pre_def g; DoChildren
 
       | GCompTag _
@@ -84,15 +96,69 @@ class printTypeDefs = object(self)
       | g -> DoChildren
 end
 
+(* Change the name of fields to safe c++ identifiers.  Also unroll typedefs
+ to avoid namespace issues *)
+let safe_field fi = 
+   {fcomp = fi.fcomp; fname = safe_cpp fi.fname;
+    ftype = unrollTypeDeep fi.ftype;
+    fbitfield = fi.fbitfield; fattr = fi.fattr; floc = fi.floc}
 
+let rec safe_fields fields =
+   match fields with
+   | fi :: rest -> safe_field fi :: safe_fields rest
+   | [] -> []
+
+let rec safe_args args =
+   match args with
+   | (name, t, attr) :: rest -> (safe_cpp name, t, attr) :: safe_args rest
+   | [] -> []
+
+(* Change pointer fields to be kptr structs *)
+let kptr_field fi =
+   fi.ftype <- make_kptr fi.ftype; fi
+
+let rec kptr_fields fields =
+   match fields with
+   | fi :: rest -> kptr_field fi :: kptr_fields rest
+   | [] -> []
+
+(* Go through all types, change structure names, field names, and
+ replace with kptrs *)
+class safeCppTypes = object(self)
+   inherit nopCilVisitor
+   method vtype (t : typ) = 
+      match t with
+      | TComp (comp, attr) ->
+         comp.cname <- safe_cpp comp.cname;
+         comp.cfields <- kptr_fields (safe_fields comp.cfields);
+         ChangeTo (TComp (comp, attr))
+      | TFun (ret, Some args, vararg, attr) ->
+         ChangeTo (TFun (ret, Some (safe_args args), vararg, attr))
+      | _ -> DoChildren
+
+   method vglob (g : global) = 
+      match g with
+      | GCompTag (comp, loc) ->
+         comp.cname <- safe_cpp comp.cname;
+         comp.cfields <- kptr_fields (safe_fields comp.cfields);
+         ChangeDoChildrenPost ([(GCompTag (comp, loc))], (fun x -> x))
+      | GType ({tname=n; ttype=t; treferenced=r}, loc) ->
+         ChangeTo ([GType ({tname=n; ttype=make_kptr t; treferenced=r}, loc)])
+      | _ -> DoChildren
+end
+
+(* Print all kptr construction macros *)
 let print_kptrs _ =
    Hashtbl.iter (fun k v -> 
       match v with 
       | KPtr (id, TVoid _, _) ->
          fprintf !out_kptrs "MAKE_VKPTR(%d, void);\n" id
       | KPtr (id, orig_type, _) -> 
+         let td_g = GType ({tname="_kptr_" ^ (string_of_int id) ^ "_t";
+                              ttype=orig_type; treferenced=false},
+                           {line=(-1); file=""; byte=(-1)}) in
          let t_str =
-            Pretty.sprint 80 (printType !printerForMaincil () orig_type) in
+            Pretty.sprint 80 (printGlobal !printerForMaincil () td_g) in
          fprintf !out_kptrs "MAKE_KPTR(%d, %s);\n" id t_str
       ) kptr_table
 
@@ -100,6 +166,6 @@ let print_typeapi cil_file =
    out_struct_list := open_out "type_list.h";
    out_pre_defs := open_out "pre_defs.h";
    out_kptrs := open_out "kptr_list.h";
-   visitCilFile (new makeKptrs) cil_file;
+   visitCilFile (new safeCppTypes) cil_file;
    visitCilFileSameGlobals (new printTypeDefs) cil_file;
    print_kptrs ();
